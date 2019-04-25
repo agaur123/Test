0a04e495057ddda56863a836ad4529e699c6d9ac
agaur123 <aman@flux7.com>
2019-04-25 16:28:51 +0530

    addinh new code

--- sqube/integrate_sonarqube.groovy
+++ sqube/integrate_sonarqube.groovy
@@ -1,20 +1,23 @@
 // This Jenkins job deploys the sonarqube
+import com.flux7.dslfactory.*
+def deploystack = new PipelineJobFactory(this)
 git_url = GIT_URL
 git_branch = GIT_BRANCH
-cf_git_url = CF_GIT_URL
-cf_git_branch = CF_GIT_BRANCH
-job_name = "deploy-sonarqube"
+job_name = "test-sonarqube"
 job_path = "${new File(__FILE__).parent}"
-def jenkinsfile  = "jobs/deploy-sonarqube/Jenkinsfile"
+def jenkinsfile  = "jobs/tests/test-sonarqube/Jenkinsfile"
 
 pipelineJob(job_name) 
 {
-  description("Jenkins pipeline job to deploy SonarQube")
+  description("Jenkins pipeline job to integrate SonarQube")
   logRotator(-1,100)
   parameters 
   {
-    stringParam("GIT_URL", git_url, description="GIT URL of repo containing ansible playbook for sonarqube")
-    stringParam("GIT_BRANCH", git_branch, description="GIT Branch of repo containing anisble playbook")
+    stringParam("SONAR_PROJECT_NAME", '', description="Name of the project for addition to SonarQube")
+    stringParam("GIT_URL", git_url, description="GIT URL of the repo to be added to sonarqube")
+    stringParam("GIT_BRANCH", git_branch, description="GIT Branch of the repo to be added to sonarqube")
+
+
   }
   logRotator(numToKeep = 100)
     definition {
